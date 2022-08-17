d845bb17d6b179d5929ea20a6b48d6e91a53809c
gp702648 <111425466+gp702648@users.noreply.github.com>
2022-08-17 17:01:43 +0530

    Add files via upload

---
+++ ternary.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("%d is =a\n",sizeof(a));
+    printf("%d is =a\n",sizeof(b));
+    printf("%d is =a\n",sizeof(c));
+    printf("%d is =a",sizeof(d));
+    printf("%d is =a",sizeof(main));
+}
