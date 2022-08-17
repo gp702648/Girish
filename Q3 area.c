6c966d9a1aae1abf2e1f755bb1dfc70dac4bf393
gp702648 <111425466+gp702648@users.noreply.github.com>
2022-08-17 16:53:05 +0530

    Add files via upload

---
+++ Q3 area.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float pi=3.14,area,r;
+    printf("enter the radius of garden\n");
+    scanf("%f",&r);
+    area=3.14*r*r;
+    printf("area that has covered=%f",area);
+}
